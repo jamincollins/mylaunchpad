f545dc306208f5ab6bbab10144e7ea44dd301d8e
Jamin W. Collins <jamin.collins@gmail.com>
2016-03-18 07:13:29 -0700

    whitespace fixups for flake8
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- appsmenu/__init__.py
+++ appsmenu/__init__.py
@@ -17,7 +17,7 @@ CACHE_DIR = "mylaunchpad"
 
 
 class MenuCache:
-    def __init__(self, tag='xdg-menu', AUTO_UPDATE = True, cache_dir = CACHE_DIR,  file_name = MENU_CACHE_FILE, xdg_menu='default'):
+    def __init__(self, tag='xdg-menu', AUTO_UPDATE=True, cache_dir=CACHE_DIR,  file_name=MENU_CACHE_FILE, xdg_menu='default'):
         self.tag = tag
         home_path = os.path.realpath(os.path.expanduser('~'))
         self.cache_dir_path = home_path + '/' + ".cache/" + cache_dir
@@ -38,48 +38,49 @@ class MenuCache:
             if self.xdg_menu == 'default':
                 menu = 'applications.menu'
             else:
-                menu = self.xdg_menu  + '.menu'
+                menu = self.xdg_menu + '.menu'
         ## xdg-menu
         if not os.path.exists('/etc/xdg/menus/' + menu):
             print('/etc/xdg/menus/'+menu + ' Not found!')
 
         # write menu cache
         self.createFile(self.file_path)
-        self.file=open(self.file_path,'a')
-        self.file.write( '<?xml version="1.0" encoding="UTF-8"?>\n')
-        self.file.write( '<' + self.tag + '>\n')
+        self.file = open(self.file_path, 'a')
+        self.file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        self.file.write('<' + self.tag + '>\n')
         try:
             map(self.walk_menu, gmenu.lookup_tree(menu).root.get_contents())
         except:
             print("ocurrio un problema")
-        self.file.write('</'+ self.tag + '>\n')
+        self.file.write('</' + self.tag + '>\n')
         self.file.close()
 
     def getMenu(self):
         #print self.file_path
-        return  self.file_path
+        return self.file_path
+
     def walk_menu(self, entry):
         if entry.get_type() == gmenu.TYPE_DIRECTORY:
-            self.file.write( '<menu id="%s" label="%s" icon="%s">\n' \
-                % (escape(entry.menu_id), escape(entry.get_name()),escape(entry.get_icon()) ) )
+            self.file.write('<menu id="%s" label="%s" icon="%s">\n' \
+                % (escape(entry.menu_id), escape(entry.get_name()), escape(entry.get_icon())))
             map(self.walk_menu, entry.get_contents())
-            self.file.write('</menu>\n' )
+            self.file.write('</menu>\n')
         elif entry.get_type() == gmenu.TYPE_ENTRY and not entry.is_excluded:
             try:
-                self.file.write( '<item label="%s" icon="%s">\n' % (escape(entry.get_name()), escape(entry.get_icon())))
+                self.file.write('<item label="%s" icon="%s">\n' % (escape(entry.get_name()), escape(entry.get_icon())))
                 command = re.sub(' [^ ]*%[fFuUdDnNickvm]', '', entry.get_exec())
                 if entry.launch_in_terminal:
                     command = 'xterm -title "%s" -e %s' % \
                         (entry.get_name(), command)
-                self.file.write( '<action name="Execute">\n' + \
-                    '<command>%s</command></action>\n' % escape(command) )
-                self.file.write( '</item>\n' )
+                self.file.write('<action name="Execute">\n' + \
+                    '<command>%s</command></action>\n' % escape(command))
+                self.file.write('</item>\n')
             except:
                 pass
             #    print "no se pudo cargar el item " + entry.get_name()
 
     def createFile(self, file_path):
         if not os.path.exists(self.cache_dir_path):
-            os.system('mkdir %s' %self.cache_dir_path)
-        file=open(file_path,'w')
+            os.system('mkdir %s' % self.cache_dir_path)
+        file = open(file_path, 'w')
         file.close()
