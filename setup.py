758b0b0c8a7ab6f6e938390713ccc2916f9fd4b5
Jamin W. Collins <jamin.collins@gmail.com>
2016-03-18 06:52:27 -0700

    removed trailing whitespace from setup.py
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import os, sys, glob, fnmatch
-try: 
+try:
     from distutils.core import setup, Extension
     import distutils.command.install_data
-    
+
 except:
     print "DistUtils / SetupTools are required"
     sys.exit(1)
@@ -14,22 +14,22 @@ try:
 except:
     print "DistUtils Extras is required"
     sys.exit(1)
-    
-          
-setup(name="mylaunchpad",  
-          version="0.4",  
-          description="Eyecandy Full Screen launcher script for openbox and other stand-alone window managers.",  
-          author="Gabriel Sabillon",  
-          author_email="gabrielsabillon@gmail.com",  
-          url="https://github.com/gaboelnuevo/mylaunchpad",  
-          license="GPL",  
-          scripts=["data/mylaunchpad"],  
-          packages=["appsmenu"], 
+
+
+setup(name="mylaunchpad",
+          version="0.4",
+          description="Eyecandy Full Screen launcher script for openbox and other stand-alone window managers.",
+          author="Gabriel Sabillon",
+          author_email="gabrielsabillon@gmail.com",
+          url="https://github.com/gaboelnuevo/mylaunchpad",
+          license="GPL",
+          scripts=["data/mylaunchpad"],
+          packages=["appsmenu"],
           data_files = [('/usr/share/icons', glob.glob('data/icons/*')),
-                 ('/usr/share/applications', glob.glob('data/mylaunchpad-menu.desktop')), 
+                 ('/usr/share/applications', glob.glob('data/mylaunchpad-menu.desktop')),
                  ('/etc', glob.glob('data/mylaunchpad.conf'))],
-    
+
           cmdclass = { 'build' : build_extra.build_extra,
                        'build_i18n' :  build_i18n.build_i18n },
 
-)  
+)
